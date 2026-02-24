b735bf20177659c8494557010857d99a7e2808ed
laurac42 <114315963+laurac42@users.noreply.github.com>
2026-02-24 17:10:24 +0000

    Checks if user is an admin

---
+++ lib/auth/role.ts
@@ -0,0 +1,22 @@
+import { createClient } from "@/lib/supabase/client";
+
+/**
+ * Checks if a user has an admin role by querying the "user_roles" table in Supabase.
+ * @param userId the ID of the user to check
+ * @returns boolean indicating whether the user is an admin or not
+ */
+export async function isAdmin() {
+    try {
+        const supabase = await createClient();
+        const { data: isAdmin, error } = await supabase.rpc('is_current_user_admin')
+
+        if (error) {
+            throw error;
+        }
+        console.log("User role data:", isAdmin);   
+        return isAdmin;
+    } catch (error) {
+        console.error("Error checking admin status:", error);
+        return false;
+    }
+}
