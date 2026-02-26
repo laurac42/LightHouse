48a206bd1789548ba6bc8f293a8aa994cdc3f23e
laurac42 <114315963+laurac42@users.noreply.github.com>
2026-02-26 14:30:58 +0000

    scrape estate agent details and add to database

---
+++ scripts/scrape_agents.ts
@@ -0,0 +1,183 @@
+// !!! run script using 'node -r ts-node/register -r tsconfig-paths/register scrape_agents.ts'
+import * as cheerio from "cheerio";
+import * as axios from "axios";
+import {config} from "dotenv";
+config({path: "../.env.local"}); // load environment variables from .env file
+import { createClient } from "@supabase/supabase-js";
+
+// create client with service role key to allow uploading to storage from a non-server environment
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.SUPABASE_SERVICE_ROLE_KEY! 
+);
+
+const TSPC_URL = "https://tspc.co.uk/solicitors/";
+
+/**
+ * Scrape the TSPC website to get a list of estate agents, then scrape each agent's page to get their address and phone number.
+ */
+async function scrapeEstateAgents() {
+    try {
+        const html = await axios.default.get(TSPC_URL).then(response => response.data);
+        // parse the HTML using cheerio
+        const $ = cheerio.load(html);
+        const $solicitors = $(".solicitor-item");
+        const $solicitorNames = $solicitors.find(".content-wrapper .title-wrapper h3");
+        const $solicitorImageURLS = $solicitors.find(".image-wrapper img").map((i, el) => $(el).attr("data-src")).get();
+
+        const solicitorData = $solicitorNames.map((index, el) => ({
+            name: $(el).text().trim(),
+            imageUrl: $solicitorImageURLS[index]
+        })).get();
+
+        // upload solicitor data to supabase
+        const { data, error } = await supabase.from("estate_agencies").insert(solicitorData.map(agent => ({
+            name: agent.name,
+            logo_url: agent.imageUrl
+        }))).select("id, name");
+
+        // match the agency name to ID, so they can be linked to the locations when those are uploaded
+        const agencyMap = new Map<string, number>();
+        data?.forEach((agency: any) => {
+            agencyMap.set(agency.name, agency.id);
+        });
+
+        if (error) {
+            throw error;
+        }
+
+        for (const element of $solicitorImageURLS) {
+            console.log("Image url: " , element);
+            downloadAgentLogo("https://tspc.co.uk" + element);
+        }
+        
+        for (const element of $solicitorNames) {
+            const regex = /[^a-z0-9-]+/gi; // groups all characters that are not letters, numbers or hyphens
+            const trimmedName = $(element).text().trim().toLowerCase().replace(regex, "-").replace(/^\-+|\-+$/g, ""); // replace groups with hyphen and convert to lowercase, then remove leading/trailing hyphens
+
+            console.log(`Scraping agent: ${trimmedName}`);
+            await scrapeSpecificAgent(trimmedName, agencyMap.get($(element).text().trim())!);
+
+        }
+    } catch (error) {
+        console.error("Error scraping estate agents:", error);
+    }
+}
+
+/**
+ * Scrape details of a specific agent given their name (as it appears in the URL)
+ * Returns the address and phone number
+ * @param agentName The name of the agent to scrape
+ * @param agencyId the ID of the agency in the database, to link the location to when uploading
+ */
+async function scrapeSpecificAgent(agentName: string, agencyId: number) {
+    try {
+        const agentUrl = TSPC_URL + '/' + agentName;
+
+        const agentResponse = await fetch(agentUrl);
+        const agentHtml = await agentResponse.text();
+        const $agentPage = cheerio.load(agentHtml);
+
+        const locations = $agentPage(".location-item").get();
+        for (const location of locations) {
+            const $location = $agentPage(location);
+            const $infoItems = $location.find(".info-item");
+
+            // email is cloudflare protected, so add emails manually and ignore here
+            const address = $infoItems.eq(0).text().trim();
+            const { addressLine1, addressLine2, city, postcode } = getAgentAddress(address);
+            const phone = $infoItems.eq(1).text().trim();
+            
+            // upload each location to the estate_agency_location table, linking to the agent in the estate_agents table
+            const { error } = await supabase.from("estate_agency_location").insert({
+                address_line_1: addressLine1,
+                address_line_2: addressLine2,
+                city: city,
+                post_code: postcode,
+                phone_number: phone,
+                estate_agency_id: agencyId
+            });
+            if (error) {
+                throw error;
+            }
+        }
+    } catch (error) {
+        console.error("Error scraping specific agent:", error);
+    }
+}
+
+/**
+ * Extracts the address, city and postcode from a given address string.
+ * @param address address to extract details from
+ * @return object containing addressLine1, addressLine2, city and postcode
+ */
+export function getAgentAddress(address: string) {
+    var postcode: string | null = "";
+    var city = "";
+    var addressLine1 = "";
+    var addressLine2 = "";
+
+    // commas are inconsistent in the addresses, so remove them
+    address = address.replace(/,/g, " ").replace(/\s+/g, " ").trim();
+
+    const postcodeRegex = /([A-Z]{1,2}\d{1,2}[A-Z]?\s*\d[A-Z]{2})/i; // regex to match UK postcodes
+    const postcodeMatch = address.match(postcodeRegex);
+    postcode = postcodeMatch ? postcodeMatch[1] : null;
+
+    const addressWithoutPostcode = postcode ? address.replace(postcode, "").trim() : address;
+
+    // last word of address is now the city
+    city = addressWithoutPostcode.split(" ").slice(-1)[0];
+    const addressWithoutCity = addressWithoutPostcode.replace(city, "").trim();
+
+    // the remaining part of the address is the street address
+    // if there is a number and it is not at the start of the address, split into 2 lines
+    const numGroups = addressWithoutCity.match(/(\d+)/g);
+
+    if (numGroups && numGroups.length > 0) {
+
+        for (const num of numGroups) {
+            const numIndex = addressWithoutCity.indexOf(num);
+            if (numIndex > 0) {
+                const al1 = addressWithoutCity.substring(0, numIndex).trim();
+                const al2 = addressWithoutCity.substring(numIndex).trim();
+                addressLine1 = al1;
+                addressLine2 = al2;
+                break;
+            } else {
+                addressLine1 = addressWithoutCity;
+            }
+        }
+    }
+
+    return {
+        addressLine1,
+        addressLine2,
+        city,
+        postcode
+    };
+}
+
+/**
+ * Downloads an agent logo from a given URL and uploads it to Supabase storage
+ * @param imageUrl the URL of the image 
+ */
+async function downloadAgentLogo(imageUrl: string) {
+    try {
+        const response = await axios.default.get(imageUrl, { responseType: "arraybuffer" });
+        const imageData = response.data;
+        const fileName = imageUrl.split("/").pop();
+
+        // upload to Supabase storage
+        const { error } = await supabase.storage.from("lighthouse-bucket").upload(`logos/${fileName}`, imageData, {
+            contentType: "image/webp"
+        });
+        if (error) {    
+            throw error;
+        }
+    } catch (error) {
+        console.error("Error downloading agent logo:", error);
+    }
+}
+
+scrapeEstateAgents();
---
+++ tests/address.test.tsx
@@ -0,0 +1,47 @@
+import { getAgentAddress } from "../scripts/scrape_agents";
+
+describe("getAgentAddress", () => {
+    it("should extract address details with one address line", () => {
+        const address = "165 High Street Arbroath DD11 1DR";
+        const result = getAgentAddress(address);
+        expect(result).toEqual({
+            addressLine1: "165 High Street",
+            addressLine2: "",
+            city: "Arbroath",
+            postcode: "DD11 1DR"
+        });
+    });
+
+    it("should handle addresses with commas", () => {
+        const address = "28 Marywell Brae, Kirriemuir DD8 4BP";
+        const result = getAgentAddress(address);
+        expect(result).toEqual({
+            addressLine1: "28 Marywell Brae",
+            addressLine2: "",
+            city: "Kirriemuir",
+            postcode: "DD8 4BP"
+        });
+    });
+
+    it("should handle addresses with multiple numbers", () => {
+        const address = "2 India Buildings 86 Bell Street Dundee DD1 1JQ";
+        const result = getAgentAddress(address);
+        expect(result).toEqual({
+            addressLine1: "2 India Buildings",
+            addressLine2: "86 Bell Street",
+            city: "Dundee",
+            postcode: "DD1 1JQ"
+        });
+    });
+
+    it("should handle addresses with multiple address lines but one number", () => {
+        const address = "Whitehall House, 33 Yeaman Shore Dundee DD1 4BJ";
+        const result = getAgentAddress(address);
+        expect(result).toEqual({
+            addressLine1: "Whitehall House",
+            addressLine2: "33 Yeaman Shore",
+            city: "Dundee",
+            postcode: "DD1 4BJ"
+        });
+    });
+});
