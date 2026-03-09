b34d4a2dd6c4c2a366da48a66eb46ab915759b81
laurac42 <114315963+laurac42@users.noreply.github.com>
2026-03-09 21:34:46 +0000

    Property data on details page

--- app/public/properties/[id]/page.tsx
+++ app/public/properties/[id]/page.tsx
@@ -1,19 +1,51 @@
 'use client';
-import { Suspense } from "react";
+import { Suspense, useEffect } from "react";
 import { use } from "react";
+import { createClient } from "@/lib/supabase/client";
+import { Database } from "@/types/supabase";
+import { error } from "console";
 
-function PropertyDetails({ params }: { params: Promise<{ id: string }> }) {
+type Property = Database["public"]["Tables"]["properties"]["Row"];
+
+function PropertyDetails({ params }: { params: Promise<{ id: number }> }) {
     const { id } = use(params);
+    const property = use(fetchPropertyDetails(id));
+
+    async function fetchPropertyDetails(id: number): Promise<Property | null> {
+        try {
+            const supabase = await createClient();
+            const { data, error } = await supabase
+                .from("properties")
+                .select("*")
+                .eq("id", id)
+                .single();
+
+            if (error) {
+                throw new Error(error.message);
+            }
+            return data;
+        } catch (error) {
+            console.error("Error fetching property details:", error);
+            return null;
+        }
+    }
+
     return (
         <div>
             <h1>Property Details</h1>
             <p>Property ID: {id}</p>
+            {property && (
+                <div>
+                    <p>Property Name: {property.title}</p>
+                    <p>Property Description: {property.description}</p>
+                </div>
+            )}
         </div>
     );
 
 }
 
-export default function Page({ params }: { params: Promise<{ id: string }> }) {
+export default function Page({ params }: { params: Promise<{ id: number }> }) {
     return (
         <Suspense fallback={<div>Loading...</div>}>
             <PropertyDetails params={params} />
