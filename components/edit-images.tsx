ba29cef20c08e1b7999da03791ebfcf421eb3136
laurac42 <114315963+laurac42@users.noreply.github.com>
2026-03-16 19:58:57 +0000

    Added floorplan category to images

--- components/edit-images.tsx
+++ components/edit-images.tsx
@@ -199,6 +199,32 @@ export default function EditImages({ params }: { params: { id: number } }) {
             </div>
 
             <div>
+                <Label className="py-2 text-lg" htmlFor="gardenImages">Floorplans</Label>
+                <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+
+                    {/** display only images with 'floorplan' prefix */}
+                    {imageUrls.filter((url) => url.startsWith('floorplan')).map((url, index) => (
+                        <div key={index} className="relative">
+                            <img src={process.env.NEXT_PUBLIC_BUCKET_URL + 'properties/' + params.id + '/' + url} alt={`Property Image ${index + 1}`} className="w-full h-48 object-cover rounded" />
+                            <button className="absolute top-2 right-2 bg-buttonColor hover:bg-buttonColor/90 text-white rounded-full p-1">
+                                <X size={16} />
+                            </button>
+                        </div>
+                    ))}
+                    {/** If there are no images, display a message */}
+                    {imageUrls.filter((url) => url.startsWith('floorplan')).length === 0 && (
+                        <p className="text-sm text-muted-foreground">No floorplans uploaded.</p>
+                    )}
+                </div>
+                <div className="flex items-center gap-4 mt-4">
+                    <Button className="bg-midBlue hover:bg-midBlue/90 text-foreground">
+                        <PlusCircleIcon size={16} />
+                        &nbsp; Add Floorplan
+                    </Button>
+                </div>
+            </div>
+
+            <div>
                 <Label className="py-2 text-lg" htmlFor="otherImages">Other Images</Label>
                 <p className="text-sm text-muted-foreground pb-2">Images that don't fit into the other categories, e.g. additional rooms, parking, views etc.</p>
                 <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
