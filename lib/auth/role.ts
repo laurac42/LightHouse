7c3168319882ba7ba130136d407bbd30274cda1e
laurac42 <114315963+laurac42@users.noreply.github.com>
2026-03-24 13:37:03 +0000

    Updating error handling

--- lib/auth/role.ts
+++ lib/auth/role.ts
@@ -5,18 +5,13 @@ import { createClient } from "@/lib/supabase/client";
  * @returns boolean indicating whether the user is an admin or not
  */
 export async function isAdmin() {
-    try {
-        const supabase = await createClient();
-        const { data: isAdmin, error } = await supabase.rpc('is_current_user_admin')
+    const supabase = await createClient();
+    const { data: isAdmin, error } = await supabase.rpc('is_current_user_admin')
 
-        if (error) {
-            throw error;
-        }
-        return isAdmin;
-    } catch (error) {
-        console.error("Error checking admin status:", error);
-        return false;
+    if (error) {
+        throw error;
     }
+    return isAdmin;
 }
 
 /**
@@ -25,18 +20,13 @@ export async function isAdmin() {
  * @returns boolean indicating whether the user is an admin or not
  */
 export async function isAdminById(userId: string) {
-    try {
-        const supabase = await createClient();
-        const { data: isAdmin, error } = await supabase.rpc('is_user_admin', { p_id: userId })
+    const supabase = await createClient();
+    const { data: isAdmin, error } = await supabase.rpc('is_user_admin', { p_id: userId })
 
-        if (error) {
-            throw error;
-        }
-        return isAdmin;
-    } catch (error) {
-        console.error("Error checking admin status:", error);
-        return false;
+    if (error) {
+        throw error;
     }
+    return isAdmin;
 }
 
 /**
@@ -44,18 +34,14 @@ export async function isAdminById(userId: string) {
  * @returns boolean indicating whether the user is an estate agent or not
  */
 export async function isEstateAgent() {
-    try {
-        const supabase = await createClient();
-        const { data: isEstateAgent, error } = await supabase.rpc('is_current_user_agent')
 
-        if (error) {
-            throw error;
-        }
-        return isEstateAgent;
-    } catch (error) {
-        console.error("Error checking estate agent status:", error);
-        return false;
+    const supabase = await createClient();
+    const { data: isEstateAgent, error } = await supabase.rpc('is_current_user_agent')
+
+    if (error) {
+        throw error;
     }
+    return isEstateAgent;
 }
 
 /**
@@ -64,18 +50,13 @@ export async function isEstateAgent() {
  * @returns boolean indicating whether the user is an agent or not
  */
 export async function isEstateAgentById(userId: string) {
-    try {
-        const supabase = await createClient();
-        const { data: isEstateAgent, error } = await supabase.rpc('is_user_estate_agent', { p_id: userId })
+    const supabase = await createClient();
+    const { data: isEstateAgent, error } = await supabase.rpc('is_user_estate_agent', { p_id: userId })
 
-        if (error) {
-            throw error;
-        }
-        return isEstateAgent;
-    } catch (error) {
-        console.error("Error checking estate agent status:", error);
-        return false;
+    if (error) {
+        throw error;
     }
+    return isEstateAgent;
 }
 
 /**
@@ -84,22 +65,18 @@ export async function isEstateAgentById(userId: string) {
  * @returns boolean indicating whether the user is a seller or not
  */
 export async function isSeller(userId: string) {
-    try {
-        const supabase = await createClient();
-        const { data: isSeller, error } = await supabase
-            .from('user_roles')
-            .select('role')
-            .eq('user_id', userId)
-            .eq('role', 'seller')
-            .maybeSingle();
+    const supabase = await createClient();
+    const { data: isSeller, error } = await supabase
+        .from('user_roles')
+        .select('role')
+        .eq('user_id', userId)
+        .eq('role', 'seller')
+        .maybeSingle();
 
-        if (error || !isSeller) {
-            throw error;
-        }
-        return isSeller ? true : false;
-    } catch (error) {
-        return false;
+    if (error || !isSeller) {
+        throw error;
     }
+    return isSeller ? true : false;
 }
 
 /** 
@@ -115,7 +92,6 @@ export async function isSellerByEmail(email: string) {
     if (error) {
         throw error;
     }
-    console.log("Fetched seller status: ", isSeller);
     return isSeller;
 }
 
@@ -125,21 +101,16 @@ export async function isSellerByEmail(email: string) {
  * @returns The ID of the agnency location the agent works at
  */
 export async function getAgentsLocationId(agentId: string) {
-    try {
-        const supabase = await createClient();
-        const { data, error } = await supabase
-            .from("estate_agent_profiles")
-            .select("estate_agency_location_id")
-            .eq("id", agentId)
-            .single();
+    const supabase = await createClient();
+    const { data, error } = await supabase
+        .from("estate_agent_profiles")
+        .select("estate_agency_location_id")
+        .eq("id", agentId)
+        .single();
 
-        if (!data || error) {
-            throw error ? error : new Error("Unable to fetch agency location ID");
-        }
-        console.log("Fetched agency location ID: ", data);
-        return data;
-    } catch (error) {
-        console.error("Error fetching agency location ID", error);
-        return null;
+    if (!data || error) {
+        throw error ? error : new Error("Unable to fetch agency location ID");
     }
+    return data;
+
 }
